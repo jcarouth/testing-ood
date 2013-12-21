a90cf54d9b49774af1f35d67b7bcb5595ea84c1b
Jeff Carouth <jcarouth@gmail.com>
2013-12-21 07:24:28 -0600

    Refactor test to use mock of \Example\Product

--- tests/Example/CartTest.php
+++ tests/Example/CartTest.php
@@ -39,8 +39,24 @@ class CartTest extends \PHPUnit_Framework_TestCase
 
     public function testSubtotalIsSumOfItemPrices()
     {
-        $this->cart->addItem(new \Example\Product(1500));
-        $this->cart->addItem(new \Example\Product(2000));
+        $productOne = $this->getMockBuilder('\\Example\\Product')
+            ->setMethods(array('getPrice'))
+            ->getMock();
+
+        $productOne->expects($this->any())
+            ->method('getPrice')
+            ->will($this->returnValue(1500));
+
+        $productTwo = $this->getMockBuilder('\\Example\\Product')
+            ->setMethods(array('getPrice'))
+            ->getMock();
+
+        $productTwo->expects($this->any())
+            ->method('getPrice')
+            ->will($this->returnValue(2000));
+
+        $this->cart->addItem($productOne);
+        $this->cart->addItem($productTwo);
         $this->assertEquals(3500, $this->cart->subtotal());
     }
 }
